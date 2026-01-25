4e3c21a2b9803b39bf72e1b8c8b39d475d1635fa
James Nowell <james.nowell@bebraven.org>
2026-01-25 14:29:38 -0600

    Fixup before release

--- src/lib/ezbot.ts
+++ src/lib/ezbot.ts
@@ -279,7 +279,7 @@ async function initEzbotWithServerSidePredictions(
     trackerConfig.crossDomainLinker = crossDomainLinkerFunction;
   }
 
-  const tracker = newTracker(ezbotTrackerId, ezbotTrackerDomain, trackerConfig);
+  const tracker = newTracker(ezbotTrackerId, ezbotTrackerDomain(projectId), trackerConfig);
   if (!tracker) {
     throw new Error('Failed to initialize tracker');
   }
